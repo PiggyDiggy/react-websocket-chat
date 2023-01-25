88d8189abe3f56788d8d413d6e4e30a1fc0edeef
PiggyDiggy <dima-gus04@mail.ru>
2023-01-25 08:25:51 +0300

    Added scroll on receiving/sending new message

--- client/src/components/MessagesList/MessagesList.tsx
+++ client/src/components/MessagesList/MessagesList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { IMessage } from "../../types";
 import { ServerMessage } from "../ServerMessage";
 import { UserMessage } from "../UserMessage";
@@ -9,8 +9,28 @@ type Props = {
 };
 
 export const MessagesList: React.FC<Props> = ({ messages }) => {
+  const listRef = useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    const list = listRef.current as HTMLUListElement;
+
+    const observer = new ResizeObserver(([entry]) => {
+      entry.target.scrollTop = entry.target.scrollHeight;
+    });
+    observer.observe(list);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    const list = listRef.current as HTMLUListElement;
+    list.scrollTo({ top: list.scrollHeight, behavior: "smooth" });
+  }, [messages]);
+
   return (
-    <ul className={css["messages-list"]}>
+    <ul className={css["messages-list"]} ref={listRef}>
       {messages.map((msg) =>
         msg.author === "server" ? (
           <ServerMessage message={msg} key={msg.id} />
